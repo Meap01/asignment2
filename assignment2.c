5f90ffd95be38026a3474331af05b34cf3f840eb
Daniel Krasovski <dkrasovskijj@gmail.com>
2019-02-20 18:53:06 +0000

    Create assignment2.c

---
+++ assignment2.c
@@ -0,0 +1,83 @@
+/* 
+
+Program by Daniel Krasovski.
+*/
+#include <stdio.h>
+#include <stdlib.h>
+
+float menu1(float);
+float menu2(float);
+float menu3(float);
+float menu4(float);
+
+int main()
+{
+	while (loop == 0)
+	{
+		printf("What would you like to do? \n \n");
+		printf("1. Enter code \n");
+		printf("2. Encrypt and verify \n");
+		printf("3. Decrypt code \n");
+		printf("4. Times code has been entered correctly and incorrectly\n");
+		printf("5. Exit program \n");
+
+		scanf("%d", &menu);
+
+		if (menu == 1)
+		{
+			
+        }
+
+		if (menu == 2)
+		{
+
+		}
+		
+		if (menu == 3)
+		{
+
+		}
+
+		if (menu == 4)
+		{
+
+		}
+
+		if (menu == 5)
+		{
+			printf("The program is now closing");
+			loop = 1;
+		}
+
+	}
+}
+
+float menu1()
+{
+	
+	
+}
+
+float menu2()
+{
+	
+	
+}
+
+float menu3()
+{
+	
+	
+}
+
+float menu4()
+{
+	
+	
+}
+
+float menu5()
+{
+	
+	
+}
