6183141bb90ef0208cba31e60da65d4c88e6f037
Daniel Krasovski <dkrasovskijj@gmail.com>
2019-02-21 17:51:01 +0000

    Update assignment2.c

--- assignment2.c
+++ assignment2.c
@@ -8,7 +8,7 @@ Program by Daniel Krasovski.
 #define SIZE 4
 
 int* menu1(int *);
-float menu2(float);
+int* menu2(int *);
 float menu3(float);
 float menu4(float);
 
@@ -32,7 +32,6 @@ int main()
 
 		if (menu == 1)
 		{
-		    
             int* ptr = menu1(pin_entered);
             
             for (int i = 0; i < SIZE; i++)
@@ -43,7 +42,13 @@ int main()
 
 		if (menu == 2)
 		{
-
+			int* ptr = menu2(pin_entered);
+			for (int i = 0; i < SIZE; i++)
+            {
+            	printf("%d ", *ptr+i); 
+            }
+			
+			
 		}
 		
 		if (menu == 3)
@@ -67,12 +72,38 @@ int main()
 
 int* menu1(int *pin_entered)
 {
+	char str[100];
 	printf("enter 4 seperate numbers \n");
 	for (int i = 0; i < SIZE; i++)
 	{
-		scanf("%[^0-9]%d", pin_entered+i); // stops the code from crashing if user enters a float number or character.
+		scanf("%[^0-9]%d",str, pin_entered+i); // stops the code from crashing if user enters a float number or character.
 	}
 
 	return pin_entered;
 }
+
+int* menu2(int *pin_entered)
+{
+	extern int encrypted_code[SIZE];
+	*encrypted_code = malloc(SIZE);
+	
+	*(encrypted_code+1) = *pin_entered+3;
+	*(encrypted_code+2) = *pin_entered+4;
+	*(encrypted_code+3) = *pin_entered+1;
+	*(encrypted_code+4) = *pin_entered+2;
+	for(int i = 0; i < SIZE; i++)
+	{	
+		*(encrypted_code+i) = *(encrypted_code+i) + 1;
+		if (*encrypted_code+i == 10)
+		{
+			*(encrypted_code+i) = 0;
+		}
+	}
+	return encrypted_code;
+	
+	
+	
+	
+}
+
 
