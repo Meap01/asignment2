549d633cf1a273aeb67f668706b76d06520c3588
Daniel Krasovski <dkrasovskijj@gmail.com>
2019-03-01 13:45:33 +0000

    Update assignment2.c

--- assignment2.c
+++ assignment2.c
@@ -1,7 +1,14 @@
 /* 
 Program by Daniel Krasovski.
 date: 24/02/2019
+
+Program Description: 
+
+Compile with: https://www.onlinegdb.com/ / https://repl.it/languages/c
+Does not compile with borland.
+
 */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -44,18 +51,8 @@ int main()
 
 		if (menu == 1)
 		{
-			int* ptr = menu1(pin_entered);
-			if (fail1 != 1)
-			{
-				option_fail1 = 1;   // allows user to go into 2nd option
-				printf("the code entered is: \n");
-				
-				for (int i = 0; i < SIZE; i++)
-				{
-					printf("%d ", *(ptr+i)); 
-				}
-			}    
-      option_fail3 = 0; // stops person from option 4, until they encrypt code
+			menu1(pin_entered);
+			option_fail3 = 1; // stops person from option 4, until they encrypt code
 		}
 
 		if (menu == 2)
@@ -66,27 +63,14 @@ int main()
 				main();  //goes back to the main menu.
 				
 			}
-			int ret;
-			ret = menu2(pin_entered);  //checks if pin entered is the same as one stored
-			option_fail2 = 1;
-			if (ret == 0)
-			{
-				printf("the code entered is Correct and has been encrypted \n");
-				correct++;
-			}
-			else
-			{
-				printf("Wrong Code entered but it has been encrypted \n");
-				incorrect++;
-			}
-      printf("The Encrypted code is: \n");
-      for( int i = 0; i <SIZE; i++)
-      {
-        printf("%d",*(encrypted_code+i));
-      }
-		  option_fail1 = 0;  // stops the user from going back straight into option 2,
-		}
-				
+
+			menu2(pin_entered); // encrypts code and checks if it is correct.
+			
+			option_fail2 = 1; // allows user to enter menu 3
+			option_fail1 = 0; // stops the user from going back straight into option 2,
+			option_fail3 = 0; // allows user to go into menu 4
+		}	
+		
 		if (menu == 3)
 		{
 			if( option_fail2 != 1)
@@ -94,31 +78,29 @@ int main()
 				printf("you must do option 2 first"); 
 				main();
 			}
-			int* ptr = menu3(encrypted_code); //decrypts the code and prints it
-			printf("the decrypted code is: "); 
-			for (int i = 0; i < SIZE; i++)
-			{
-				printf("%d ", *(ptr+i)); 
-			}
-    option_fail2 = 0; //stops the user from going back into option 3
+			
+			menu3(encrypted_code); //decrypts the code and prints it
+			
+			option_fail2 = 0; //stops the user from going back into option 3
 		}
 
 		if (menu == 4)
 		{
-      if( option_fail3 == 1)
+			if( option_fail3 == 1)
 			{
 				printf("you must encrypt the code first"); 
 				main();
-      }
+			}
+			
 			printf("the code has been entered %d times correctly and %d times incorrectly \n",correct, incorrect);
-      option_fail2 = 1;
+			
 		}
 
 		if (menu == 5)
 		{
 			printf("The program is now closing");
 			loop = 1;
-      break;
+			break;
 		}
 
 	}
@@ -145,7 +127,7 @@ int* menu1(int *pin_entered)
 			printf("failure, enter a number less than 10\n");
 			i-=1;
 		}
-    if(pin_entered[i] < 0)
+		if(pin_entered[i] < 0)
 		{
 			printf("failure, enter a greater than -1\n");
 			i-=1;
@@ -153,6 +135,18 @@ int* menu1(int *pin_entered)
 		i++;
 	}
 	fail1 = 0;
+	
+	if (fail1 != 1)
+	{
+		option_fail1 = 1;   // allows user to go into 2nd option
+		printf("the code entered is: \n");
+		
+		for (int i = 0; i < SIZE; i++)
+		{
+			printf("%d ", *(pin_entered+i)); 
+      
+		}
+	}    
 	return pin_entered;
 }
 
@@ -165,7 +159,8 @@ int menu2(int *pin_entered)
 	*(encrypted_code+3) = *(pin_entered+1);
 
 	for(int i = 0; i < SIZE; i++)  //encryption algorithm
-	{	
+	{
+    *(pin_entered+i) = 0; // overwrites pin_entered to not store the unencrypted pin
 		*(encrypted_code+i) +=1;
 
 		if (*(encrypted_code+i) == 10)
@@ -177,8 +172,26 @@ int menu2(int *pin_entered)
   
 	printf("\n");
 	
+	printf("The Encrypted code is: \n");
+	
+	for( int i = 0; i <SIZE; i++)
+	{
+		printf("%d",*(encrypted_code+i));
+	}
+  printf("\n");
 	int ret;
 	ret = memcmp(encrypted_code, access_code,sizeof(access_code)); // compares the the encrypted pin with the stored pin
+  	
+	if (ret == 0) // if the pin entered is correct to the one stored
+	{
+		printf("The code entered is Correct and has been encrypted \n");
+		correct++;
+	}
+	else //if pin entered is incorrect to the one  stored
+	{
+		printf("Wrong Code entered but it has been encrypted \n");
+		incorrect++;
+	}
 	return ret;
 	
 }
@@ -197,6 +210,12 @@ int* menu3(int *encrypted_code)
 		{
 			*(pin_entered+i) = 9;
 		}
+	}
+	printf("the decrypted code is: "); 
+	
+	for (int i = 0; i < SIZE; i++)
+	{
+		printf("%d ", *(pin_entered+i)); 
 	}
   return pin_entered;
 }
